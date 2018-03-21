a3f66589c91bd2957d32f0d1daaa9a4311c487c6
Issa Boubacar <bacaris78@gmail.com>
2018-03-21 17:29:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   object.key = value
   return recipes
 }
-function 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  var updatedObject = object.key = value;
+  return updatedObject
+}
